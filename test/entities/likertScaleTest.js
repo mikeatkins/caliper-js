b0af151b7644841b5c36fba19e3a21379a0a96bc
Anthony Whyte <anthwhyte@gmail.com>
2019-03-18 10:07:04 -0400

    Update tests with isOrderedSelection and scalePoints property name changes.

--- test/entities/likertScaleTest.js
+++ test/entities/likertScaleTest.js
@@ -49,7 +49,7 @@ testUtils.readFile(path, function(err, fixture) {
     
     var entity = entityFactory().create(LikertScale, {
       id: "https://example.edu/scale/2",
-      points: 4,
+      scalePoints: 4,
       question: "Do you agree with the opinion presented?",
       itemLabels: labels,
       itemValues: values
